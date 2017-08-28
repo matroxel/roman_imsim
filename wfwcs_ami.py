48e14e9c80e8645d360414035965f49531dcd752
troxel <michael.a.troxel@gmail.com>
2017-08-28 12:57:03 -0400

    push

--- wfwcs_ami.py
+++ wfwcs_ami.py
@@ -77,7 +77,7 @@ sca_c = np.loadtxt('c.txt')
 
 fig = plt.figure(figsize=(12,5))
 ax = fig.add_subplot(131)
-sc=ax.scatter(ra_vals, dec_vals, sca, s=1, 
+sc=ax.scatter(ra_vals, dec_vals, c=sca.astype(float), s=1, 
               lw=0, cmap=plt.cm.viridis)
 # The previous line is a change to make defaults like the newer matplotlib
 # since the Ohio Supercomputer Center comp seems to have an older mpl by default
@@ -90,7 +90,7 @@ xlim = ax.get_xlim()
 ylim = ax.get_ylim()
 
 ax2 = fig.add_subplot(132)
-sc2 = ax2.scatter(ra_vals, dec_vals, sca_ch, s=1,
+sc2 = ax2.scatter(ra_vals, dec_vals, c=sca_ch.astype(float), s=1,
                   lw=0, cmap=plt.cm.viridis)
 ax2.scatter([ra_cen], [dec_cen], c='w', marker='o', s=40)
 plt.xlabel('RA')
@@ -101,7 +101,7 @@ ax2.set_xlim(xlim)
 ax2.set_ylim(ylim)
 
 ax3 = fig.add_subplot(133)
-sc2 = ax3.scatter(ra_vals, dec_vals, sca_c, s=1,
+sc2 = ax3.scatter(ra_vals, dec_vals, c=sca_c.astype(float), s=1,
                   lw=0, cmap=plt.cm.viridis)
 ax3.scatter([ra_cen], [dec_cen], c='w', marker='o', s=40)
 plt.xlabel('RA')
