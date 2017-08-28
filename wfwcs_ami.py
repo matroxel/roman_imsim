6a104c70c10344f190c3454130aa80ca4e720682
troxel <michael.a.troxel@gmail.com>
2017-08-28 15:10:54 -0400

    push

--- wfwcs_ami.py
+++ wfwcs_ami.py
@@ -58,6 +58,9 @@ for i in range(len(ra_vals)):
     sca.append(wf.findSCA(wcs, galsim.CelestialCoord(ra=ra_vals[i]*galsim.radians,
                                                     dec=dec_vals[i]*galsim.radians)))
 sca=np.array(sca)
+for i in range(ra_vals):
+    if sca[i] is None:
+        sca[i]=0
 print np.min(sca_ch),np.max(sca_ch)
 np.savetxt('galsim.txt',sca.astype(int))
 
