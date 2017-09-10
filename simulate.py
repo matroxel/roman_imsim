fd3108146d61bf7610a3ca67ad5dcf8748e49ac3
troxel <michael.a.troxel@gmail.com>
2017-09-10 18:19:39 -0400

    push

--- simulate.py
+++ simulate.py
@@ -248,7 +248,7 @@ class wfirst_sim(object):
             if (os.path.isfile(filename))&(~self.params['rerun_models']):
 
                 phot       = fio.FITS(self.params['gal_sample'])[-1]['fwhm','redshift',filter_flux_dict[self.params['filter']]]
-                pind_list_ = np.ones(fits.read_header()['NAXIS2']).astype(bool) # storage list for original index of photometry catalog
+                pind_list_ = np.ones(len(phot)).astype(bool) # storage list for original index of photometry catalog
                 pind_list_ = pind_list_&(phot[filter_flux_dict[self.params['filter']]]<99)&(phot[filter_flux_dict[self.params['filter']]]>0) # remove bad mags
                 pind_list_ = pind_list_&(phot['redshift']>0)&(phot['redshift']<5) # remove bad redshifts
                 pind_list_ = pind_list_&(phot['fwhm']*2.*0.06/wfirst.pixel_scale<16) # remove large objects to maintain 32x32 stamps
