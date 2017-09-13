2e378b0f5ebebaf6b634a9a9be4c313f721f8b27
troxel <michael.a.troxel@gmail.com>
2017-09-12 20:05:11 -0400

    push

--- simulate.py
+++ simulate.py
@@ -565,7 +565,7 @@ class wfirst_sim(object):
         # galaxy_sed   = self.galaxy_sed.atRedshift(self.store['z'][ind]) # redshift SED
         galaxy_sed   = self.galaxy_sed.withMagnitude(self.store['mag'][ind],self.bpass) # apply correct flux from magnitude
         gal          = gal * galaxy_sed * galsim.wfirst.collecting_area * galsim.wfirst.exptime
-        print gal.calculateFlux(self.bpass),galsim.wfirst.collecting_area * galsim.wfirst.exptime # store flux
+        print gal.calculateFlux(self.bpass), self.store['size'][ind], self.store['mag'][ind] # store flux
         gal          = galsim.Convolve(gal, self.PSF) # Convolve with PSF and append to final image list
 
         # Get local wcs solution at galaxy position in SCA.
