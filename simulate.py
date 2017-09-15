e6953b76613eff99efea58d1ab136bb97b615ae4
troxel <michael.a.troxel@gmail.com>
2017-09-14 22:41:27 -0400

    add full image plane support

--- simulate.py
+++ simulate.py
@@ -840,9 +840,8 @@ class wfirst_sim(object):
             for i,ind in enumerate(star_use_ind):
                 radec    = galsim.CelestialCoord(self.stars['ra'][ind]*galsim.radians,self.stars['dec'][ind]*galsim.radians)
                 star_sed = galsim.SED(sedpath_Star, wave_type='nm', flux_type='flambda')
-                print self.stars.dtype.names
                 star,xy  = self.star(star_sed, 
-                                    flux = self.stars[self.params['filter']][ind], 
+                                    flux = self.stars['flux'][ind], 
                                     radec = radec, 
                                     bound = [-self.params['stamp_size'],wfirst.n_pix+self.params['stamp_size']],
                                     return_xy = True)
