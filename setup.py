e6d9d1b1c28cb9069d621ef74f431fd0ef9b90ba
troxel <michael.a.troxel@gmail.com>
2018-06-08 14:02:09 -0400

    structuring

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='wfirst_imsim',
+   version='0.0',
+   description='Image simulations suite for WFIRST',
+   license="MIT",
+   author='Michael Troxel',
+   author_email='michael.a.troxel@gmail.com',
+   url="",
+   packages=['wfirst_imsim'],
+   install_requires=['ngmix', 'fitsio', 'astropy', 'mpi4py', 'meds', 'yaml', 'healpy', 'numpy', 'logging', 'matplotlib', 'pylab'],
+)
