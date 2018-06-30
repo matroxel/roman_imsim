7403ef990d06165255fada1c9efaa1412cd983bc
troxel <michael.a.troxel@gmail.com>
2018-06-30 17:45:56 -0400

    push

--- simulate_new.py
+++ simulate_new.py
@@ -1374,7 +1374,7 @@ class draw_image():
         """
 
         # Get star model with given SED and flux
-        self.star_model(sed=self.star_sed,flux=self.star['flux'][0])
+        self.star_model(sed=self.star_sed,flux=self.star['flux'])
 
         # Create postage stamp bounds for star
         b = galsim.BoundsI( xmin=self.xyI.x-int(self.params['stamp_size'])/2,
