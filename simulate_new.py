d31941efb9e07d2ce239fea76c3636106bec5a59
troxel <michael.a.troxel@gmail.com>
2018-07-01 14:04:30 -0400

    push

--- simulate_new.py
+++ simulate_new.py
@@ -1475,6 +1475,7 @@ class draw_image():
 
         # Create star postage stamp
         star_stamp = galsim.Image(b, wcs=self.pointing.WCS)
+        print self.st_model.gsparams
 
         # Draw star model into postage stamp
         self.st_model.drawImage(image=star_stamp,offset=self.offset,method='no_pixel')
