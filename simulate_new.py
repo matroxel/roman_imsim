78aeab1cc90e6c040f703d8ed082f1501ba79852
troxel <michael.a.troxel@gmail.com>
2018-07-02 23:44:07 -0400

    push

--- simulate_new.py
+++ simulate_new.py
@@ -1523,7 +1523,7 @@ class draw_image():
 
         star_stamp.write('/fs/scratch/cond0083/wfirst_sim_out/images/'+str(self.ind)+'.fits.gz')
 
-        Add star stamp to SCA image
+        # Add star stamp to SCA image
         self.im[b&self.b] = self.im[b&self.b] + star_stamp[b&self.b]
         # self.st_model.drawImage(image=self.im,add_to_image=True,offset=self.xy-self.im.true_center,method='phot',rng=self.rng,maxN=1000000)
         # print 'done',self.ind
