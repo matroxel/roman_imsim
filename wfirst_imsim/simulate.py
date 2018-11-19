04095bfa7927f948dfa261cd64c59f54c9eecc36
troxel <michael.a.troxel@gmail.com>
2018-11-19 12:49:54 -0500

    push

--- wfirst_imsim/simulate.py
+++ wfirst_imsim/simulate.py
@@ -1828,7 +1828,6 @@ class accumulate_output():
             self.index_= None
             self.index['ra']  = np.degrees(self.index['ra'])
             self.index['dec'] = np.degrees(self.index['dec'])
-            self.index = 
             fio.write(index_filename,self.index,clobber=True)
 
         if setup:
