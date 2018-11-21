91756464b914be0b2069272e41cbb2b6976ca58b
troxel <michael.a.troxel@gmail.com>
2018-11-21 13:26:11 -0500

    push

--- wfirst_imsim/simulate.py
+++ wfirst_imsim/simulate.py
@@ -2465,8 +2465,13 @@ class wfirst_sim(object):
                 print 'Saving stamp dict to '+filename
                 save_obj(gals, filename )
 
-            # Build indexing table for MEDS making later
-            index_table = np.zeros(len(gals),dtype=[('ind',int), ('sca',int), ('dither',int), ('x',float), ('y',float), ('ra',float), ('dec',float), ('mag',float), ('stamp',int)])
+                # Build indexing table for MEDS making later
+                index_table = np.zeros(len(gals),dtype=[('ind',int), ('sca',int), ('dither',int), ('x',float), ('y',float), ('ra',float), ('dec',float), ('mag',float), ('stamp',int)])
+
+            else:
+                
+                return
+
             i=0
             for gal in gals:
                 index_table['ind'][i]    = gals[gal]['ind']
