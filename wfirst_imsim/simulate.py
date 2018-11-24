8cf984b7cb210248e99a9663f9f691a1df3d81e0
troxel <michael.a.troxel@gmail.com>
2018-11-23 22:53:25 -0500

    push

--- wfirst_imsim/simulate.py
+++ wfirst_imsim/simulate.py
@@ -3059,10 +3059,9 @@ if __name__ == "__main__":
         sim.setup(filter_,dither,setup=True)
         sys.exit()
     elif dither=='meds':
-        if len(sys.argv)>4:
-            if sys.argv[4]=='setup':
-                setup = True
-                pix = -1
+        if sys.argv[4]=='setup':
+            setup = True
+            pix = -1
         else:
             setup = False
             if (sim.params['meds_from_file'] is not None) & (sim.params['meds_from_file'] != 'None'):
