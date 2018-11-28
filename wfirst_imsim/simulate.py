e615fcae17be36fbd9a3a6716edc42630407aa94
troxel <michael.a.troxel@gmail.com>
2018-11-28 10:51:23 -0500

    push

--- wfirst_imsim/simulate.py
+++ wfirst_imsim/simulate.py
@@ -2169,7 +2169,7 @@ class accumulate_output_disk():
                 if len(j)==0:
                     j = 0
                 else:
-                    j = np.max(j)
+                    j = np.max(j)+1
                 index_i = np.where((self.index['ind']==gals[gal]['ind'])&(self.index['dither']==gals[gal]['dither']))[0]
                 assert len(index_i)==1
                 index_i=index_i[0]
