0617499c82b6c744beb4d907e633683834ba2e73
Michael Troxel <michael.troxel@duke.edu>
2020-07-03 12:17:46 -0400

    push

--- wfirst_imsim/__init__.py
+++ wfirst_imsim/__init__.py
@@ -7,6 +7,18 @@ Copyright (c) 2012-2017 by the GalSim developers team on GitHub
 https://github.com/GalSim-developers
 """
 
+from __future__ import division
+from __future__ import print_function
+
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+from builtins import range
+from past.builtins import basestring
+from builtins import object
+from past.utils import old_div
+
+
 from .simulate import wfirst_sim
 from .output import accumulate_output_disk
 from .image import draw_image 
@@ -28,16 +40,6 @@ from .misc import get_filename
 from .misc import get_filenames
 from .misc import write_fits
 
-from __future__ import division
-from __future__ import print_function
-
-from future import standard_library
-standard_library.install_aliases()
-from builtins import str
-from builtins import range
-from past.builtins import basestring
-from builtins import object
-from past.utils import old_div
 import numpy as np
 import healpy as hp
 import sys, os, io
