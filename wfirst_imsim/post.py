09c2c9f66cb9c4c5ef1a609ae9ead813c634cca2
Michael Troxel <michael.troxel@duke.edu>
2020-08-07 16:44:40 -0400

    push

--- wfirst_imsim/post.py
+++ wfirst_imsim/post.py
@@ -369,6 +369,9 @@ class postprocessing(wfirst_sim):
                 mask = np.append(mask,self.near_coadd(ra_max,dec_min))
                 mask = np.append(mask,self.near_coadd(ra_max,dec_max))
                 mask = np.unique(mask)
+                print (ra[i],dec[i],len(mask))
+                if len(mask)==0:
+                	continue
 
                 input_list = []
                 for x in mask:
