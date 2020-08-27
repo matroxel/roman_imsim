ff9d2518a5fd0b879cd67ce389bf209083e5d508
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-08-27 18:37:38 -0400

    dcc debug

--- wfirst_imsim/misc.py
+++ wfirst_imsim/misc.py
@@ -197,10 +197,10 @@ def hsm(im, psf=None, wt=None):
         out['e2'] = shear.g2
 
     return out
-
+"""
 def get_filename( out_path, path, name, var=None, name2=None, ftype='fits', overwrite=False, make=True ):
     """
-    Helper function to set up a file path, and create the path if it doesn't exist.
+    #Helper function to set up a file path, and create the path if it doesn't exist.
     """
     if var is not None:
         name += '_' + var
@@ -217,6 +217,27 @@ def get_filename( out_path, path, name, var=None, name2=None, ftype='fits', over
     if (overwrite)&(os.path.exists(filename)):
         os.remove(filename)
     return filename
+"""
+
+def get_filenames( out_path, path, name, var=None, name2=None, ftype='fits', exclude = None ):
+    """
+    Helper function to set up a file path, and create the path if it doesn't exist.
+    """
+    if var is not None:
+        name += '_' + var
+    if name2 is not None:
+        name += '_' + name2
+    name += '*.' + ftype
+    fpath = os.path.join(out_path,path)
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+    if not os.path.exists(fpath):
+        os.mkdir(fpath)
+    filename = os.path.join(fpath,name)
+    if exclude is None:
+        return glob.glob(filename)
+    else:
+        return [ x for x in glob.glob(filename) if exclude not in x ]
 
 def get_filenames( out_path, path, name, var=None, name2=None, ftype='fits' ):
     """
