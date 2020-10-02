ab3d86efab8daf99c9f0842651ea177c439a3eb9
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-10-02 17:49:29 -0400

    plot the stamp

---
+++ wfirst_imsim/stamp_plot.py
@@ -0,0 +1,22 @@
+import astropy
+import pickle
+import io
+
+filename='/Users/masayamn/Desktop/dcc_output/fiducial_H158_22531_1_0.cPickle'
+filename1='/Users/masayamn/Desktop/dcc_output/fiducial_H158_22531_1_1.cPickle'
+
+
+with io.open(filename, 'rb') as p :
+    unpickler = pickle.Unpickler(p)
+    while p.peek(1) :
+        gal1 = unpickler.load()
+        if gal1['ind']==901802:
+            gal_stamp1=gal1
+        
+with io.open(filename1, 'rb') as p2 :
+    unpickler = pickle.Unpickler(p2)
+    while p2.peek(1) :
+        gal2 = unpickler.load()
+        if gal2['ind']==901802:
+            gal_stamp2=gal2
+print(gal_stamp1, gal_stamp2)
