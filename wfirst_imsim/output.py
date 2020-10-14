db7e601d2e62ac137f8b9ddd00e898918c76d29b
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-10-14 13:21:20 -0400

    included None as the first element of psf

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -125,6 +125,7 @@ class accumulate_output_disk(object):
                 st_model = galsim.Convolve(st_model, psf_sca)
                 st_model.drawImage(image=psf_stamp)
                 self.all_psfs.append(psf_stamp)
+            self.all_psfs.insert(0,None)
             #print(self.all_psfs)
 
             #if not condor:
@@ -1674,7 +1675,6 @@ Queue ITER from seq 0 1 4 |
             t   = truth[ind]
 
             sca_list = m[ii]['sca']
-            print(i, ii, sca_list)
             m2 = [self.all_psfs[j].array for j in sca_list]
             obs_list,psf_list,included,w = self.get_exp_list(m,ii,m2=m2,size=t['size'])
             if len(included)==0:
