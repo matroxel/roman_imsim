e374f5b46cc6945ee682f5afe2ced41eb70af2ca
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-12-15 22:40:15 -0500

    bunch of prints

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -1014,9 +1014,9 @@ Queue ITER from seq 0 1 4 |
             print(m['orig_row'][i][jj], m['orig_start_row'][i][jj], m['cutout_row'][i][jj], m['box_size'][i], box_size)
             print(m['orig_col'][i][jj], m['orig_start_col'][i][jj], m['cutout_col'][i][jj], m['box_size'][i], box_size)
             print((m['orig_row'][i][jj]-m['orig_start_row'][i][jj])-m['box_size'][i]/2+box_size/2)
-            print((m['orig_col'][i][j]-m['orig_start_col'][i][j])-m['box_size'][i]/2+box_size/2)
-            offset_x = (m['orig_row'][i][jj]-m['orig_start_row'][i][jj])-m['box_size'][i]/2+box_size/2#m['cutout_col'][i][jj] - m['box_size'][i]/2
-            offset_y = (m['orig_col'][i][j]-m['orig_start_col'][i][j])-m['box_size'][i]/2+box_size/2#m['cutout_row'][i][jj] - m['box_size'][i]/2
+            print((m['orig_col'][i][jj]-m['orig_start_col'][i][jj])-m['box_size'][i]/2+box_size/2)
+            offset_x = m['cutout_col'][i][jj] - m['box_size'][i]/2
+            offset_y = m['cutout_row'][i][jj] - m['box_size'][i]/2
             print(offset_x, offset_y)
             offset = galsim.PositionD(offset_x, offset_y)
             m2[jj].drawImage(image=psf_stamp, offset=offset)
