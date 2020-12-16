47f664114f57c75d31a9682a43f73d672c8482da
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-12-15 22:31:53 -0500

    bunch of prints

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -1012,6 +1012,9 @@ Queue ITER from seq 0 1 4 |
             psf_stamp = galsim.Image(b, scale=wfirst.pixel_scale)
             box_size = get_stamp(size,m['box_size'][i])
             print(m['orig_row'][i][jj], m['orig_start_row'][i][jj], m['cutout_row'][i][jj], m['box_size'][i], box_size)
+            print(m['orig_col'][i][jj], m['orig_start_col'][i][jj], m['cutout_col'][i][jj], m['box_size'][i], box_size)
+            print((m['orig_row'][i][jj]-m['orig_start_row'][i][jj])-m['box_size'][i]/2+box_size/2)
+            print((m['orig_col'][i][j]-m['orig_start_col'][i][j])-m['box_size'][i]/2+box_size/2)
             offset_x = (m['orig_row'][i][jj]-m['orig_start_row'][i][jj])-m['box_size'][i]/2+box_size/2#m['cutout_col'][i][jj] - m['box_size'][i]/2
             offset_y = (m['orig_col'][i][j]-m['orig_start_col'][i][j])-m['box_size'][i]/2+box_size/2#m['cutout_row'][i][jj] - m['box_size'][i]/2
             print(offset_x, offset_y)
