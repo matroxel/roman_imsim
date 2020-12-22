1e36af9d93bc0c7de575745562f3e2e6d883f5ea
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-12-22 13:03:52 -0500

    oversampling psf test 4

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -2035,8 +2035,8 @@ Queue ITER from seq 0 1 4 |
                 #print('coadd',coadd[i].noise)
                 #print('There are '+str(len(obs_list))+' observations for this object.')
                 print(i, t['size'], time.time()-t0)
-                np.savetxt('/hpc/group/cosmology/masaya/roman_imsim/wfirst_imsim/coadd_image_new8_'+str(i)+'.txt', coadd.image)
-                np.savetxt('/hpc/group/cosmology/masaya/roman_imsim/wfirst_imsim/coadd_psf_new8_'+str(i)+'.txt', coadd.psf.image)
+                np.savetxt('/hpc/group/cosmology/masaya/roman_imsim/wfirst_imsim/coadd_image_new4_'+str(i)+'.txt', coadd.image)
+                np.savetxt('/hpc/group/cosmology/masaya/roman_imsim/wfirst_imsim/coadd_psf_new4_'+str(i)+'.txt', coadd.psf.image)
                 np.savetxt('/hpc/group/cosmology/masaya/roman_imsim/wfirst_imsim/coadd_image_old_'+str(i)+'.txt', old_coadd.image)
                 np.savetxt('/hpc/group/cosmology/masaya/roman_imsim/wfirst_imsim/coadd_psf_old_'+str(i)+'.txt', old_coadd.psf.image)
                 
