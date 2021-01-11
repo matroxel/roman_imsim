d8a9808f2c6a0160958e1448209f9baf47b27cac
Masaya Yamamoto <masayamn@masaya-mac.home>
2021-01-11 12:17:34 -0500

    psf_obs2 to psf_obs

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -1086,9 +1086,9 @@ Queue ITER from seq 0 1 4 |
 
             psf_obs = Observation(im_psf, jacobian=gal_jacob, meta={'offset_pixels':None,'file_id':None})
             psf_obs2 = Observation(im_psf2, jacobian=psf_jacob2, meta={'offset_pixels':None,'file_id':None})
-            #obs = Observation(im, weight=weight, jacobian=gal_jacob, psf=psf_obs, meta={'offset_pixels':None,'file_id':None})
+            obs = Observation(im, weight=weight, jacobian=gal_jacob, psf=psf_obs, meta={'offset_pixels':None,'file_id':None})
             # oversampled PSF
-            obs = Observation(im, weight=weight, jacobian=psf_jacob2, psf=psf_obs, meta={'offset_pixels':None,'file_id':None})
+            #obs = Observation(im, weight=weight, jacobian=psf_jacob2, psf=psf_obs, meta={'offset_pixels':None,'file_id':None})
             obs.set_noise(noise)
 
             obs_list.append(obs)
