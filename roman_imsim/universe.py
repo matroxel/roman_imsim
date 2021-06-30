4d5780a53d685f03f9db6ee5059dd88e2118515b
Michael Troxel <michael.troxel@duke.edu>
2021-06-30 14:13:32 -0400

    osg stuff

--- roman_imsim/universe.py
+++ roman_imsim/universe.py
@@ -280,6 +280,25 @@ class init_catalogs(object):
     def get_near_sca(self,chunk=1000000):
 
         print('memory check',self.gals)
+
+        self.gal_ind  = self.pointing.near_pointing( self.gals['ra'][:], self.gals['dec'][:] )
+        # print len(self.gal_ind),len(self.gals['ra'][:])
+        if len(self.gal_ind)==0:
+            self.gal_ind = []
+            self.gals = []
+        else:
+            self.gals = self.gals[self.gal_ind]
+
+        mask_sca      = self.pointing.in_sca(self.gals['ra'][:],self.gals['dec'][:])
+        if len(mask_sca)==0:
+            self.gal_ind = []
+            self.gals = []
+        else:
+            self.gals    = self.gals[mask_sca]
+            self.gal_ind = self.gal_ind[mask_sca]
+
+        print(self.gal_ind)
+
         n = fio.FITS('dc2_truth_gal.fits')[-1].read_header()['NAXIS2']
         self.gal_ind = []
         for i in range(0,n,chunk):
@@ -298,24 +317,6 @@ class init_catalogs(object):
 
         self.gal_ind = gal_ind
         self.gals    = self.gals[self.gal_ind]
-        print(self.gal_ind)
-
-        self.gal_ind  = self.pointing.near_pointing( self.gals['ra'][:], self.gals['dec'][:] )
-        # print len(self.gal_ind),len(self.gals['ra'][:])
-        if len(self.gal_ind)==0:
-            self.gal_ind = []
-            self.gals = []
-        else:
-            self.gals = self.gals[self.gal_ind]
-
-        mask_sca      = self.pointing.in_sca(self.gals['ra'][:],self.gals['dec'][:])
-        if len(mask_sca)==0:
-            self.gal_ind = []
-            self.gals = []
-        else:
-            self.gals    = self.gals[mask_sca]
-            self.gal_ind = self.gal_ind[mask_sca]
-
         print(self.gal_ind)
 
         self.star_ind = self.pointing.near_pointing( self.stars['ra'][:], self.stars['dec'][:] )
