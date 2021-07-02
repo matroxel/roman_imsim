15535e216ece5af8cf81ec6cc525c8375bb4f7be
Michael Troxel <michael.troxel@duke.edu>
2021-07-02 14:50:32 -0400

    osg stuff

--- roman_imsim/image.py
+++ roman_imsim/image.py
@@ -609,7 +609,8 @@ class draw_image(object):
 
         # Draw galaxy model into postage stamp. This is the basis for both the postage stamp output and what gets added to the SCA image. This will obviously create biases if the postage stamp is too small - need to monitor that.
         t0 = time.time()
-        self.gal_model.drawImage(self.pointing.bpass,image=gal_stamp,offset=self.xy-b.true_center,method='phot',rng=self.rng)
+        # self.gal_model.drawImage(self.pointing.bpass,image=gal_stamp,offset=self.xy-b.true_center,method='phot',rng=self.rng)
+        self.gal_model.drawImage(image=gal_stamp,offset=self.xy-b.true_center,method='phot',rng=self.rng)
         print('--------',flux,time.time()-t0)
 
         # gal_stamp.write(str(self.ind)+'.fits')
