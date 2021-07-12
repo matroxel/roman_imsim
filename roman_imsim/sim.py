469e8879957dc84b6189c63d8fbff93c399ed9c0
Michael Troxel <michael.troxel@duke.edu>
2021-07-12 10:43:16 -0400

    osg stuff

--- roman_imsim/sim.py
+++ roman_imsim/sim.py
@@ -641,7 +641,7 @@ class roman_sim(object):
                                     name2=str(self.pointing.sca),
                                     ftype='fits.gz',
                                     overwrite=True)
-            self.draw_image = draw_detector(self.params, self.pointing, self.modify_image, None,  self.logger, rank=self.rank, comm=self.comm, im=im)
+            self.draw_image = draw_detector(self.params, self.pointing, self.modify_image,  self.logger, rank=self.rank, comm=self.comm, im=im)
             img,err,dq = self.draw_image.finalize_sca()
             write_fits(imfilename,img,err,dq,self.pointing.sca,self.params['output_meds'])
 
