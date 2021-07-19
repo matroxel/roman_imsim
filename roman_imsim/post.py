c7ecd47029e18c8e4d8a59c018b47231ee551c42
Michael Troxel <michael.troxel@duke.edu>
2021-07-19 18:15:26 -0400

    push

--- roman_imsim/post.py
+++ roman_imsim/post.py
@@ -618,7 +618,7 @@ class postprocessing(roman_sim):
         AstroDrizzle(list(input_list),
                      output=filename_,
                      num_cores=1,
-                     runfile='',
+                     runfile='drizzle.log',
                      context=True,
                      build=True,
                      preserve=False,
@@ -633,7 +633,7 @@ class postprocessing(roman_sim):
                      final_rot=0.,
                      final_scale=self.final_scale,
                      in_memory=False,
-                     final_wht_type='ERR',
+                     #final_wht_type='ERR',
                      combine_type='median')
 
         os.system('gzip '+filename_)
