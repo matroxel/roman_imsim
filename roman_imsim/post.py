2045a67a46252ad91e6e8d90d9a0b5ec8b710e87
Michael Troxel <michael.troxel@duke.edu>
2021-07-20 15:28:50 -0400

    push

--- roman_imsim/post.py
+++ roman_imsim/post.py
@@ -630,7 +630,7 @@ class postprocessing(roman_sim):
                      final_dec=coaddlist['coadd_dec'],
                      final_rot=0.,
                      final_scale=self.final_scale,
-                     in_memory=False,
+                     in_memory=True,
                      #final_wht_type='ERR',
                      combine_type='median')
 
