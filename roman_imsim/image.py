3890a36d4fba738a7964b6617ee74821e22c4d8c
Michael Troxel <michael.troxel@duke.edu>
2021-07-22 12:22:55 -0400

    push

--- roman_imsim/image.py
+++ roman_imsim/image.py
@@ -777,7 +777,7 @@ class draw_image(object):
 
         # Convolve with PSF
         if mag<10:
-            psf = self.pointing.load_psf(self.xyI,pupil_bin=2)
+            psf = self.pointing.load_psf(self.xyI,pupil_bin=1)
             psf = psf.withGSParams(galsim.GSParams(folding_threshold=5e-5,maximum_fft_size=16384 ))
         elif mag<12:
             psf = self.pointing.load_psf(self.xyI,pupil_bin=2)
