6f00a45b2df0641c5f9ab43384e437967cc68cbc
Michael Troxel <michael.troxel@duke.edu>
2021-07-30 11:23:36 -0400

    push

--- roman_imsim/image.py
+++ roman_imsim/image.py
@@ -177,9 +177,9 @@ class draw_image(object):
         self.gal_stamp = None
         self.weight    = None
 
-        # if self.gal_iter>0:
-        #     self.gal_done = True
-        #     return
+        if self.gal_iter>0:
+            self.gal_done = True
+            return
 
         # if self.gal_iter%1000==0:
         #     print('Progress '+str(self.rank)+': Attempting to simulate galaxy '+str(self.gal_iter)+' in SCA '+str(self.pointing.sca)+' and dither '+str(self.pointing.dither)+'.')
@@ -777,7 +777,7 @@ class draw_image(object):
         # self.st_model  = self.st_model.withFlux(flux) # reapply correct flux
 
         # Convolve with PSF
-        if mag<-99:
+        if mag<-10:
             psf = self.pointing.load_psf(self.xyI,pupil_bin=1)
             psf = psf.withGSParams(galsim.GSParams(folding_threshold=1e-4))
         elif mag<12:
