19757eab54626b1c105d1332a868afb7a34b4233
Michael Troxel <michael.troxel@duke.edu>
2021-07-30 11:35:41 -0400

    push

--- roman_imsim/image.py
+++ roman_imsim/image.py
@@ -777,8 +777,8 @@ class draw_image(object):
         # self.st_model  = self.st_model.withFlux(flux) # reapply correct flux
 
         # Convolve with PSF
-        if mag<-10:
-            print('doing pupil bin 1')
+        if mag<10:
+            print('doing pupil bin 1',mag)
             psf = self.pointing.load_psf(self.xyI,pupil_bin=1)
             psf = psf.withGSParams(galsim.GSParams(folding_threshold=1e-4))
         elif mag<12:
