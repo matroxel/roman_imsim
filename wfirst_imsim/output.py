478d9f8ae66e44a5a7d2b5d2fc51885c952c898a
Masaya Yamamoto <my137@duke.edu>
2021-08-02 11:40:35 +0900

    coadd H

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -2338,7 +2338,7 @@ Queue ITER from seq 0 1 4 |
                 else:
                     ilabel = self.shape_iter
                 filename = get_filename(self.params['out_path'],
-                                    'ngmix/coadd_oversample',
+                                    'ngmix/new_coadd_oversample',
                                     self.params['output_meds'],
                                     var=self.pointing.filter+'_'+str(self.pix)+'_'+str(ilabel)+'_mcal_coadd_'+str(metacal_keys[j]),
                                     ftype='fits',
