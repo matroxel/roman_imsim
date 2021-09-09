e23b5d52e53220a9beed431e01906240252c0ccc
Michael Troxel <michael.troxel@duke.edu>
2021-09-09 15:38:20 -0400

    push

--- roman_imsim/post.py
+++ roman_imsim/post.py
@@ -281,9 +281,9 @@ class postprocessing(roman_sim):
         self.setup_pointing()
         dither = np.loadtxt(self.params['dither_from_file'])
         limits = np.ones((len(dither),2))*-999
-        for i,d in enumerate(dither.astype(int)):
+        for i,(d,sca) in enumerate(dither.astype(int)):
             if i%100==0:
-                print(i,d,sca,start_row)
+                print(i,d,sca)
             self.update_pointing(dither=d,sca=sca,psf=False)
             limits[i,0] = self.pointing.radec.ra/galsim.degrees
             limits[i,1] = self.pointing.radec.dec/galsim.degrees
