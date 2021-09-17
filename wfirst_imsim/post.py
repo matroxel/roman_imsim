c5f853131138d1919bd0dbbbff66dba6fa1f119e
Masaya Yamamoto <my137@duke.edu>
2021-09-17 11:27:22 -0400

    bug

--- wfirst_imsim/post.py
+++ wfirst_imsim/post.py
@@ -709,7 +709,7 @@ class postprocessing(wfirst_sim):
                 continue
             b = np.binary_repr(c)[::-1]
             bi = np.array([b[i] for i in range(len(b))],dtype=int)
-            print(d_list, bi, len(d_list), len(bi))
+            print(c, b, d_list, bi, len(d_list), len(bi))
             bi = np.pad(bi, (0, len(d_list)-len(bi)), 'constant').astype(int)
             psf_coadd = galsim.Add([psf_images[d][sca] for d,sca in zip(d_list[bi],sca_list[bi])])
             psf_stamp = galsim.Image(b_psf, wcs=wcs)
