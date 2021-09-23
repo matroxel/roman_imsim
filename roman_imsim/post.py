09604e4007fcac8fbeb54662e005c859d8fafb7a
Michael Troxel <michael.troxel@duke.edu>
2021-09-23 16:19:14 -0400

    push

--- roman_imsim/post.py
+++ roman_imsim/post.py
@@ -682,7 +682,7 @@ class postprocessing(roman_sim):
                                 ftype='fits',
                                 overwrite=True)
 
-        ctx = fio.FITS(filename_)['CTX'].read().astype('uint32')
+        ctx = fio.FITS(filename_)['CTX'].read()
         if len(np.shape(ctx))>2:
             nplane = np.shape(ctx)[0]
         else:
@@ -747,8 +747,23 @@ class postprocessing(roman_sim):
     def get_coadd_psf_stamp(self,coadd_file,coadd_psf_file,x,y,stamp_size,oversample_factor=1):
 
         xy = galsim.PositionD(x,y)
-        ctx = fio.FITS(coadd_file)['CTX'][round(x),round(y)]
-        psf_coadd = galsim.InterpolatedImage(coadd_psf_file,hdu=ctx,x_interpolant='lanczos5')
+
+        hdr = fio.FITS(filename_)['CTX'].read_header()
+        if hdr['NAXIS']==3:
+            nplane = 2
+        else:
+            nplane = 1
+        if nplane<2:
+            ctx = fio.FITS(filename_)['CTX'][int(x),int(y)].astype('uint32')
+        elif nplane<3:
+            ctx = np.left_shift(fio.FITS(filename_)['CTX'][1,int(x),int(y)].astype('uint64'),32)+fio.FITS(filename_)['CTX'][0,int(x),int(y)].astype('uint32')
+        else:
+            # if nplane>2:
+            #     for i in range(nplane-2):
+            #         cc += np.left_shift(ctx[i+2,:,:].astype('uint64'),32*(i+2))
+            print('Not designed to work with more than 64 images.')
+
+        psf_coadd = galsim.InterpolatedImage(coadd_psf_file,hdu=str(ctx),x_interpolant='lanczos5')
         b_psf = galsim.BoundsI( xmin=1,
                         ymin=1,
                         xmax=stamp_size*oversample_factor,
