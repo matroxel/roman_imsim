df6d121751d26ae3663f049329f1dfad78899906
Masaya Yamamoto <my137@duke.edu>
2021-11-14 11:03:16 -0500

    multiband final

--- wfirst_imsim/output.py
+++ wfirst_imsim/output.py
@@ -2923,7 +2923,7 @@ Queue ITER from seq 0 1 4 |
                 else:
                     ilabel = self.shape_iter
                 filename = get_filename(self.params['out_path'],
-                                    'ngmix/single_multiband_3filter_final',
+                                    'ngmix/multiband_coadd_3filter_final',
                                     self.params['output_meds'],
                                     var=self.pointing.filter+'_'+str(self.pix)+'_'+str(ilabel)+'_mcal_coadd_'+str(metacal_keys[j]),
                                     ftype='fits',
