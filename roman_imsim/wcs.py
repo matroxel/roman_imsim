929d3a17714190a0cb2bf38241c4f6a6ab20af18
Troxel <michael.troxel@duke.edu>
2023-09-20 18:56:14 -0400

    debugging

--- roman_imsim/wcs.py
+++ roman_imsim/wcs.py
@@ -1,22 +1,23 @@
 import galsim
 import galsim.roman as roman
 from galsim.config import WCSBuilder,RegisterWCSType
+from galsim.angle import Angle
 
 class RomanWCS(WCSBuilder):
 
     def buildWCS(self, config, base, logger):
 
         req = { 'SCA' : int,
-                'ra'  : float,
-                'dec' : float,
-                'pa'  : float,
+                'ra'  : Angle,
+                'dec' : Angle,
+                'pa'  : Angle,
                 'mjd' : float
               }
 
         kwargs, safe = galsim.config.GetAllParams(config, base, req=req)
-        pointing = CelestialCoord(ra=kwargs['ra']*galsim.degrees, dec=kwargs['dec']*galsim.degrees)
+        pointing = CelestialCoord(ra=kwargs['ra'], dec=kwargs['dec'])
         wcs = roman.getWCS(world_pos        = pointing,
-                                PA          = kwargs['pa']*galsim.degrees,
+                                PA          = kwargs['pa'],
                                 date        = Time(kwargs['mjd'],format='mjd').datetime,
                                 SCAs        = kwargs['SCA'],
                                 PA_is_FPA   = True
