498317c81a7220651cdbaf469ff41f1f71035fe6
Troxel <michael.troxel@duke.edu>
2023-09-26 17:59:59 -0400

    debugging

--- roman_imsim/stamp.py
+++ roman_imsim/stamp.py
@@ -333,9 +333,9 @@ class Roman_stamp(StampBuilder):
 
 # Pick the right function to be _fix_seds.
 if galsim.__version_info__ < (2,5):
-    Roman_stamp._fix_seds = Roman_stamp._fix_seds_24
+    Roman_stamp.fix_seds = Roman_stamp._fix_seds_24
 else:
-    Roman_stamp._fix_seds = Roman_stamp._fix_seds_25
+    Roman_stamp.fix_seds = Roman_stamp._fix_seds_25
 
 
 # Register this as a valid type
