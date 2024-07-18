cb9488079eb344afd3de71c0b018017e5e605ca1
Arun Kannawadi <arun.kannawadi@duke.edu>
2024-07-18 13:03:59 -0400

    Avoid having to pass a dummy filename

--- det.py
+++ det.py
@@ -4,7 +4,12 @@ import sys
 params = sys.argv[1]
 visit = int(sys.argv[2])
 sca = int(sys.argv[3])
-dither_from_file = sys.argv[4]
+if len(sys.argv) > 4:
+    dither_from_file = sys.argv[4]
+else:
+    import warnings
+    warnings.warn("Ditherlist not provided. Persistence effect will not simulated.")
+    dither_from_file = None
 
 if len(sys.argv)>5:
     sca_path = sys.argv[5]
