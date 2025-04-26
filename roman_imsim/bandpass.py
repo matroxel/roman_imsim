151bcae13a40aeea19e59295cc526b6afd992c2d
Arun Kannawadi <kj.arun.kj@gmail.com>
2025-04-26 16:07:02 -0400

    Include all bands for Prism

--- roman_imsim/bandpass.py
+++ roman_imsim/bandpass.py
@@ -23,7 +23,7 @@ class RomanBandpassBuilder(BandpassBuilder):
         kwargs, safe = GetAllParams(config, base, req=req)
 
         name = kwargs['name']
-        bandpass = roman.getBandpasses(red_limit=2000)[name]
+        bandpass = roman.getBandpasses(red_limit=2000, include_all_bands=True)[name]
 
         return bandpass, safe
 
