32854e78c27068c99c02edf3829e1090d349b7c9
Yuedong Fang <yuedong0607@gmail.com>
2026-02-27 17:50:48 -0500

    add coadd level simulation

--- roman_imsim/wcs.py
+++ roman_imsim/wcs.py
@@ -1,35 +1,44 @@
 import galsim
 import romanisim.models as models
-from astropy.time import Time
+from galsim.config import WCSBuilder, RegisterWCSType
 from galsim.angle import Angle
 from galsim.celestial import CelestialCoord
-from galsim.config import RegisterWCSType, WCSBuilder
 
 
 class RomanWCS(WCSBuilder):
     def buildWCS(self, config, base, logger):
 
-        req = {
-            "SCA": int,
-            "ra": Angle,
-            "dec": Angle,
-            "pa": Angle,
-            "mjd": float,
-        }
-        opt = {"max_sun_angle": float, "force_cvz": bool}
+        if base["image"]["type"] == "roman_sca":
+            req = {
+                "SCA": int,
+                "ra": Angle,
+                "dec": Angle,
+                "pa": Angle,
+                "mjd": float,
+            }
+            opt = {"max_sun_angle": float, "force_cvz": bool}
 
-        kwargs, safe = galsim.config.GetAllParams(config, base, req=req, opt=opt)
-        if "max_sun_angle" in kwargs:
-            models.parameters.max_sun_angle = kwargs["max_sun_angle"]
-            models.wcs_utils.max_sun_angle = kwargs["max_sun_angle"]
-        pointing = CelestialCoord(ra=kwargs["ra"], dec=kwargs["dec"])
-        wcs = models.wcs_utils.getWCS(
-            world_pos=pointing,
-            PA=kwargs["pa"],
-            date=Time(kwargs["mjd"], format="mjd").datetime,
-            SCAs=kwargs["SCA"],
-            PA_is_FPA=True,
-        )[kwargs["SCA"]]
+            kwargs, safe = galsim.config.GetAllParams(config, base, req=req, opt=opt)
+            if "max_sun_angle" in kwargs:
+                models.parameters.max_sun_angle = kwargs["max_sun_angle"]
+                models.wcs_utils.max_sun_angle = kwargs["max_sun_angle"]
+            pointing = CelestialCoord(ra=kwargs["ra"], dec=kwargs["dec"])
+            wcs = models.wcs_utils.getWCS(
+                world_pos=pointing,
+                PA=kwargs["pa"],
+                date=Time(kwargs["mjd"], format="mjd").datetime,
+                SCAs=kwargs["SCA"],
+                PA_is_FPA=True,
+            )[kwargs["SCA"]]
+        elif base["image"]["type"] == "roman_coadd":
+            # req = {'coadd_file': str,
+            #        }
+            # opt = {}
+            # kwargs, safe = galsim.config.GetAllParams(
+            #     config, base, req=req, opt=opt)
+            # wcs = galsim.FitsWCS(kwargs['coadd_file'])
+            wcs = galsim.GSFitsWCS(file_name=base["image"]["coadd_file"], hdu=0)
+
         return wcs
 
 
