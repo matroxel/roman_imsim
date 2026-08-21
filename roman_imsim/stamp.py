7c5823c9be4f642cce83a2f994f9ae24764769bd
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2026-08-21 13:26:09 -0400

    remove folding_threshold

--- roman_imsim/stamp.py
+++ roman_imsim/stamp.py
@@ -112,7 +112,7 @@ class Roman_stamp(StampBuilder):
                 # # Get storead achromatic PSF
                 # psf = galsim.config.BuildGSObject(base, 'psf', logger=logger)[0]['achromatic']
                 # obj = galsim.Convolve(gal_achrom, psf).withFlux(self.flux)
-                obj = gal_achrom.withGSParams(galsim.GSParams(stepk_minimum_hlr=20, folding_threshold=0.001))
+                obj = gal_achrom.withGSParams(galsim.GSParams(stepk_minimum_hlr=20))
                 image_size = obj.getGoodImageSize(models.parameters.pixel_scale)
                 image_size = max(image_size, config.get("min_size", 0))
 
